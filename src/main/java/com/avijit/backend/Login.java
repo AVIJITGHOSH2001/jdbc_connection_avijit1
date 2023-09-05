ff3b9b3f480d16e9d57f45e07e53b227846cfb5a
AVIJITGHOSH2001 <avijitghoshcollege.com>
2023-09-05 20:02:39 +0530

    avijit_jdbc1

---
+++ src/main/java/com/avijit/backend/Login.java
@@ -0,0 +1,43 @@
+package com.avijit.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class Login extends HttpServlet
+{
+@Override
+protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+{
+String myemail = req.getParameter("email1");
+String mypass =req.getParameter("pass1");
+
+PrintWriter out = resp.getWriter();
+
+if(myemail.equals("avijit@gmail.com") && mypass.equals("avijit123"))
+{
+	//req.setAttribute("name_key","Avijit Ghosh");
+	 HttpSession session = req.getSession();
+	 session.setAttribute("name_key", "Avijit ghosh");
+	
+	RequestDispatcher rd = req.getRequestDispatcher("/profile.jsp");
+    rd.forward(req, resp);
+
+}
+else {
+resp.setContentType("text/html"); //print poper error html
+out.print("<h3 style='color:red'> Email id and password didnt match</h3>");
+RequestDispatcher rd = req.getRequestDispatcher("/index1.html");
+//rd.forward(req, resp);
+rd.include(req, resp);
+}
+}
+}
---
+++ src/main/webapp/profile.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+String myname = (String) session.getAttribute("name_key");
+%>
+<h3>Welcome : <%= myname %> </h3>
+
+<a href="home.jsp"> Home </a> &nbsp;&nbsp;
+<a href="About-us.jsp"> About Us </a>
+<a href="LogOut"> Logout </a>
+</body>
+</html>
